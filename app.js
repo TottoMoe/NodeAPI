22749b864b08e55f810a13d73c21f9d231e92c4a
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2023-02-15 18:59:51 -0800

    middleware

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-
+const morgan = require("morgan");
 
 // bring in routes
-const { getPosts } = require('./routes/post')
+const { getPosts } = require("./routes/post");
 
+// middleware
+const myOwnMiddleware = (req, res, next) => {
+  console.log("Middleware applied!!!");
+  next();
+}
+app.use(morgan("dev"));
+app.use(myOwnMiddleware)
+//************************ */
 
-app.get('/', getPosts);
+app.get("/", getPosts);
 
-const port = 3000
-
+const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Node js API is listing on port: ${port}`)
-})
+  console.log(`Node js API is listing on port: ${port}`);
+});
