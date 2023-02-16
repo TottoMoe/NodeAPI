3d882efedee912b4e9ae609b0f092c6491e61618
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2023-02-15 20:41:02 -0800

    mogoose and mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const morgan = require("morgan");
+const dotenv = require("dotenv");
+dotenv.config();
+
+// db
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => console.log("DB Connected"));
+
+mongoose.connection.on("error", (err) => {
+  console.log(`DB connection error: ${err.message}`);
+});
 
 // bring in routes
 const postRoutes = require("./routes/post");
@@ -13,10 +23,9 @@ const postRoutes = require("./routes/post");
 // app.use(myOwnMiddleware)
 
 app.use(morgan("dev"));
-
 app.use("/", postRoutes);
 
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`Node js API is listing on port: ${port}`);
